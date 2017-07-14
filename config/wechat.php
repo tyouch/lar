738d33f10b5b5a504a87781606e709c76186b7f2
赵曜 <you.ch@foxmail.com>
2017-07-14 18:26:14 +0800

    wechat.php

--- config/wechat.php
+++ config/wechat.php
@@ -7,10 +7,5 @@
  */
 
 return [
-    'AppID'         => 'wxaa8613a62ef8e9e4',
-    'AppSecret'     => '01032e6069013e3f2171e48dbfd559df',
-    'redirectUri'   => $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'] . '/wetest/', //lar/public/
-    'wPayUrl'       => 'https://api.mch.weixin.qq.com/pay/unifiedorder',
-    'mchID'         => '1484736102',
-    'apiKey'        => 'I1sifX1iA973c6N19rXXIOvvFv9HP7Fw'
+    
 ];
